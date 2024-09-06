5b63b67703d86e613e4ead9b28c6c688c932c69d
yashPundhir <yash.pundhir.prof@gmail.com>
2024-09-07 04:25:06 +0530

    do the following changes: UI updated add validations to form add modal after form submission add framer motion and react hot toast to the prject

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,58 @@
-// App.js
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Body from './components/Body';
-import CTAForm from './components/CTAForm';
-import MenuPage from './pages/MenuPage';
-import AboutPage from './pages/AboutPage';
-import ContactPage from './pages/ContactPage';
-import DishesPage from './pages/DishesPage';
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { motion } from "framer-motion";
+
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import CTAForm from "./components/CTAForm";
+import MenuPage from "./pages/MenuPage";
+// import AboutPage from "./pages/AboutPage";
+// import ContactPage from "./pages/ContactPage";
+import DishesPage from "./pages/DishesPage";
+import Footer from "./components/Footer";
 
 const App = () => {
-  const [showForm, setShowForm] = useState(false);
+	const [showForm, setShowForm] = useState(false);
 
-  const toggleForm = () => setShowForm(!showForm);
+	const toggleForm = () => setShowForm(!showForm);
 
-  return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Hero toggleForm={toggleForm} />} />
-        <Route path="/menu" element={<MenuPage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/contact" element={<ContactPage />} />
-        <Route path="/menu/:categoryName" element={<DishesPage />} />
-      </Routes>
+	return (
+		<Router>
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<Hero toggleForm={toggleForm} />} />
+				<Route path="/cuisineTypes" element={<MenuPage />} />
+				{/* <Route path="/about" element={<AboutPage />} />
+				<Route path="/contact" element={<ContactPage />} /> */}
+				<Route path="/menu/:categoryName" element={<DishesPage />} />
+			</Routes>
 
-      {showForm && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
-          <div className="relative bg-white p-6 rounded shadow-lg">
-            <button
-              className="absolute top-2 right-2 text-gray-600 hover:text-gray-900"
-              onClick={toggleForm}
-            >
-              ✖
-            </button>
-            <CTAForm />
-          </div>
-        </div>
-      )}
-      <Body />
-    </Router>
-  );
+			{showForm && (
+				<motion.div
+					initial={{ opacity: 0, scale: 0.5 }}
+					animate={{ opacity: 1, scale: 1 }}
+					transition={{
+						duration: 0.8,
+						delay: 0.05,
+						ease: [0, 0.71, 0.2, 1.01],
+					}}
+					className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center"
+				>
+					<div className="relative bg-white p-6 rounded shadow-lg">
+						<button
+							className="absolute top-2 right-3 text-gray-600 hover:text-gray-900"
+							onClick={toggleForm}
+						>
+							✖
+						</button>
+						<CTAForm toggleForm={toggleForm} />
+					</div>
+				</motion.div>
+			)}
+
+			<Footer />
+		</Router>
+	);
 };
 
 export default App;
--- src/pages/MenuPage.jsx
+++ src/pages/MenuPage.jsx
@@ -1,51 +1,78 @@
 // pages/MenuPage.js
 
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import Card from '../components/Card';
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import Card from "../components/Card";
 
 const MenuPage = () => {
-  const [categories, setCategories] = useState([]);
-  const [error, setError] = useState(null);
+	const [categories, setCategories] = useState([]);
+	const [error, setError] = useState(null);
 
-  useEffect(() => {
-    const fetchCategories = async () => {
-      try {
-        const response = await fetch('https://www.themealdb.com/api/json/v1/1/categories.php');
-        if (!response.ok) {
-          throw new Error('Failed to fetch categories');
-        }
+	useEffect(() => {
+		const fetchCategories = async () => {
+			try {
+				const response = await fetch(
+					"https://www.themealdb.com/api/json/v1/1/categories.php"
+				);
+				if (!response.ok) {
+					throw new Error("Failed to fetch categories");
+				}
 
-        const data = await response.json();
-        setCategories(data.categories);
-      } catch (error) {
-        setError(error.message);
-      }
-    };
+				const data = await response.json();
+				console.log(data);
+				setCategories(data.categories);
+			} catch (error) {
+				setError(error.message);
+			}
+		};
 
-    fetchCategories();
-  }, []);
+		fetchCategories();
+	}, []);
 
-  if (error) {
-    return <h2 className="text-center">{error}</h2>;
-  }
+	if (error) {
+		return (
+			<div className="flex flex-col justify-center items-center gap-2">
+				<span className="text-[24px] font-semibold">Something Went Wrong.</span>
+				<span className="text-[24px] font-semibold">
+					Please Try Again Later
+				</span>
+			</div>
+		);
+	}
 
-  return (
-    <section id="menu" className="py-16">
-      <h1 className="text-3xl font-bold text-center pb-[50px]">Our Menu</h1>
-      <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-        {categories.map((category) => (
-          <Link key={category.idCategory} to={`/menu/${category.strCategory.toLowerCase().replace(/\s+/g, '')}`}>
-            <Card
-              name={category.strCategory}
-              description={category.strCategoryDescription}
-              image={category.strCategoryThumb}
-            />
-          </Link>
-        ))}
-      </div>
-    </section>
-  );
+	return (
+		<section id="menu" className="py-16 bg-white">
+			{categories.length === 0 ? (
+				<div className="flex flex-col justify-center items-center">
+					<span className="text-[24px] font-semibold w-full text-center">
+						Please Wait Loading Content...
+					</span>
+				</div>
+			) : (
+				<div className="max-w-7xl mx-auto px-10">
+					<h1 className="text-3xl font-bold text-center pb-[50px]">
+						Available Cuisine Types
+					</h1>
+					<div className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+						{categories.map((category) => (
+							<Link
+								key={category.idCategory}
+								to={`/menu/${category.strCategory
+									.toLowerCase()
+									.replace(/\s+/g, "")}`}
+							>
+								<Card
+									name={category.strCategory}
+									description={category.strCategoryDescription.slice(0, 100)}
+									image={category.strCategoryThumb}
+								/>
+							</Link>
+						))}
+					</div>
+				</div>
+			)}
+		</section>
+	);
 };
 
 export default MenuPage;
